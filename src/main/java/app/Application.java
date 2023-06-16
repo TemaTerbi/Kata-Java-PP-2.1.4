b7fa914db8fe1587391148942ff75e0fb8dfbd7a
TemaTerbi <tematerbi@mail.ru>
2023-06-16 15:24:56 +0300

    Task done

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,8 +2,11 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Timer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.Time;
 
 public class Application {
 
